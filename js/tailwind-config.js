b95e770427adcf9f79a81dd3b6aa850b8ed9e394
AryanIRN <86324996+AryanIRN@users.noreply.github.com>
2026-02-21 08:36:22 +0100

    Colorpalet

---
+++ js/tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'desert-taupe': '#bfb5b3', 
+                'taupe-light': '#e5e4df',  
+                'taupe-dark': '#504a4a',   
+                'rich-black': '#000000',   
+                'soft-cream': '#e5e4df',   
+                'off-white': '#ffffff',    
+            },
+            fontFamily: {
+                display: ['"Cormorant Garamond"', 'serif'],
+                sans: ['Montserrat', 'sans-serif'],
+            },
+        }
+    }
+}
